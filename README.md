e74b25cae2f974e82ace0ec65ef958fa9072a89c
Codeliner <codeliner96@gmail.com>
2021-03-25 23:53:29 +0900

    Created test endpoint.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World API
+
+This is simple REST API powered by Spring boot.
---
+++ src/main/java/com/codeliner/helloworldApi/HelloworldApiApplication.java
@@ -0,0 +1,16 @@
+package com.codeliner.helloworldApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloworldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApiApplication.class, args);
+	}
+
+}
